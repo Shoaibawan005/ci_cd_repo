e0539db0a1a030c892474009ed37783c0811e09b
Shoaibawan005 <shoaibabbas479@gmail.com>
2025-08-22 14:53:48 +0500

    modify the host

--- index.js
+++ index.js
@@ -40,6 +40,10 @@ app.get("/api/data", (req, res) => {
 });
 
 // Start server
-app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
+// app.listen(PORT, () => {
+//   console.log(`🚀 Server running at http://localhost:${PORT}`);
+// });
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running at http://0.0.0.0:${PORT}`);
 });
